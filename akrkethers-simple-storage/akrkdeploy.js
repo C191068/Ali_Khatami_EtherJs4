b42daf80efba40dc4743d6d647ee31e04635dc02
iiuc lid lab58 <khatamiiiuc49@gmail.com>
2023-07-18 12:57:50 +0600

    a

--- akrkethers-simple-storage/akrkdeploy.js
+++ akrkethers-simple-storage/akrkdeploy.js
@@ -7,12 +7,25 @@ async function main() {
     "http://127.0.0.1:7545"
   );
 
-  const abi = fs.readFileSync();
-
   const wallet = new ethers.Wallet(
     "0x1cd5a540d56cb150d4b870ab0cb624b1181b0885aa0e12ea2e46ca74515b5b33",
     provider
   );
+
+  const abi = fs.readFileSync(
+    "./akrkSimplestorage_sol_akrkSimplestorage.abi",
+    "utf8"
+  );
+
+  const binary = fs.readFileSync(
+    "./akrkSimplestorage_sol_akrkSimplestorage.bin",
+    "utf8"
+  );
+
+  const contractfactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Executing,please keep patience...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 // http://127.0.0.1:7545
 
